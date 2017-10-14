a1401c0e0fb0e66bf37e7c7ec133844c5839660a
nicabach <nixonbarasa30@gmail.com>
2017-10-14 15:56:01 +0300

    Java first application

---
+++ first.java
@@ -0,0 +1,24 @@
+
+package meth;
+
+
+public class first {
+    
+    
+    int first1 =17;
+    int second=66;
+    
+   int first2 (int num1,int num2){
+        this.first1=num1;
+        this.second=num2;
+     
+      int c=num1+num2;
+        System.out.println("the result is "+c);
+        
+        
+        return num1+num2;
+   
+    }
+
+    
+}
