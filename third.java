861d60cf9af2afd578b6de4065ab0fc155ca7bb7
nicabach <nixonbarasa30@gmail.com>
2017-10-14 18:52:16 +0300

    Third java application

---
+++ third.java
@@ -0,0 +1,26 @@
+
+package meth;
+
+
+public class third {
+    
+    
+    
+    public int  Dev(int num1,int num2){
+    
+       int c=num1/num2;
+    
+        System.out.println(c);
+    
+    return num1/num2;
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+}
