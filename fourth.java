5bc8cf42a0c8c2fef45967b4e9151c684a0efd21
nicabach <nixonbarasa30@gmail.com>
2017-10-14 16:04:26 +0300

    Meth class java application

---
+++ fourth.java
@@ -0,0 +1,22 @@
+
+package meth;
+
+
+public class fourth {
+    
+    
+    public static int sub(int num1,int num2){
+    
+    int c=num1-num2;
+        System.out.println(num1-num2);
+        
+       
+        
+        
+        
+    return num1-num2;
+    
+    
+    }
+    
+}
